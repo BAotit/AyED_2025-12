c6acfbbf77d8ec70da39bedc4838d9f2db02ca19
BAotit <brisatito9@gmail.com>
2025-04-24 14:51:09 -0300

    Add files via upload

---
+++ 12_TITO_J/0_PC12_0/0_PC12_0/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC12_0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* ETAPA 0 -TP 0
+            Imprimir Hola mundo
+            */
+            Console.WriteLine("Hola mundo!");
+            
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ 12_TITO_J/0_PC12_1/0_PC12_1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC12_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Caso 1:
+        TIPOdato nombredevariable;
+        nombredevariable = Dato;
+
+    Caso 2 : 
+    TipoDAto nombrevariable = Dato;
+    */
+            //Caso 1
+            string variable1;//DEclaramos la variable1
+            variable1 = "Hola mundo!";//Instamos la variable1
+            Console.WriteLine(variable1);
+            Console.ReadKey();
+            //CASo2
+            string variable1 =" HolaMundo"
+        }
+    }
+}
