2c8a1d84c7625181da70edcc388d29727def3e2f
BAotit <brisatito9@gmail.com>
2025-04-24 15:02:28 -0300

    Add files via upload

---
+++ 12_TITO_J/1_PC12_TP3/1_PC12_TP3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC12_TP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            Console.WriteLine(" Ingrese un numero entero: ");
+            numero = int.Parse(Console.ReadLine());
+            
+            if (numero == 0)
+                
+            {
+                Console.WriteLine("el numero es neutro");
+                
+            }
+            else if((numero % 2) == 1)
+            {
+                Console.WriteLine("el numero es impar");
+            }
+            else if ((numero % 2) == 0)
+            {
+                Console.WriteLine("El numero es par");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 12_TITO_J/1_PC12_TP4/1_PC12_TP4/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_PC12_TP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad, ingresomensual;
+            Console.WriteLine("Ingresa tu edad: ");
+            edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa tus ingresos mensuales: ");
+            ingresomensual = int.Parse(Console.ReadLine());
+
+            if ((edad > 19) && (ingresomensual <= 100000))
+            {
+                Console.WriteLine("Puedes cobrar la beca ");
+            }
+            else if ((edad < 19) && (ingresomensual == 0))
+            {
+                Console.WriteLine("Puedes cobrar la beca ");
+            }
+            else if ((edad == 19) && (ingresomensual <= 50000))
+            {
+                Console.WriteLine("Puedes cobrar la beca ");
+            }
+            else
+            {
+                Console.WriteLine("No puedes cobrar la beca ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
