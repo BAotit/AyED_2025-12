af70fb9092718910cb4355485a99db4d66534ea5
BAotit <brisatito9@gmail.com>
2025-04-24 16:06:10 -0300

    Add files via upload

---
+++ 12_GUTIERREZ_M/0_PC12_TP4/0_PC12_TP4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0_PC12_TP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int resultado, num1, num2;
+            Console.WriteLine("introduce dos numeros:");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+            resultado = num1 + num2;
+            Console.WriteLine("resultado: "+ resultado);
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
