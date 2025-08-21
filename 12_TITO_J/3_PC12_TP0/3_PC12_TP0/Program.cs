c762b9ff1db3fc1d3a06da613d99470f5ea7c40d
BAotit <brisatito9@gmail.com>
2025-08-21 15:05:00 -0300

    Add files via upload

---
+++ 12_TITO_J/3_PC12_TP0/3_PC12_TP0/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_PC12_TP0
+{
+    class Program
+    {
+     
+        /*
+            Entrada: (Nombre) 
+Proceso: Concatenar "Hola " + "Nombre"
+Salida: (String)
+            */
+            //Funcion
+            static string Saludo(string nombre)
+            {
+                return "Hola " + nombre + ", bienvenido a C#!";
+            }
+
+            //Ejecuta
+            static void Main(string[] args)
+            {
+
+            string nombre;
+  
+            Console.WriteLine("Ingresa un nombre");
+            nombre = Console.ReadLine();
+            string mensaje = Saludo(nombre);
+            Console.WriteLine(mensaje);
+
+            Console.ReadKey();
+
+            }
+
+        }  
+}
